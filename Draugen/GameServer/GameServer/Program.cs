77d1f5d179cf20f6ee4d8f0890b8afe76324ffdb
Armin Kemura <52348946+brinobre@users.noreply.github.com>
2020-05-18 10:27:48 +0200

    added server scripts, re-arranged files in unity

---
+++ Draugen/GameServer/GameServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+            Server.Start(4, 26950);
+            Console.ReadKey();
+        }
+    }
+}
